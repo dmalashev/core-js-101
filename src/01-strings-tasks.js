322c79ccdd592437c5157fa16ce33f3d304ceb18
Dmitry Malashev <dmalashev@gmail.com>
2024-10-22 22:52:23 +0300

    feat: solve the isString task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -260,8 +260,14 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string') {
+    return true;
+  }
+  if (typeof value === 'object') {
+    return value instanceof String;
+  }
+  return false;
 }
 
 
